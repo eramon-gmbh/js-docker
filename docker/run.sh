ed6f495291db35cd80421c6cd38e433938927d0e
Sandy Schirmer <sandy.lewanscheck@eramon.de>
2020-10-22 12:25:41 +0200

    for the normal run the cmdline container is not needed - init should only be used when initializing a completely new installation; net=host is the default for now

--- docker/run.sh
+++ docker/run.sh
@@ -11,16 +11,18 @@ fi
 # shellcheck source=docker/variables.sh
 source "${DIR}"/docker/variables.sh
 
-echo "Run jasperserver pro cmdline image."
-docker run -d --env-file="${DIR}"/.env \
-    --name "${JASPERSERVER_PRO_CMDLINE_CONTAINER_NAME}" \
-    -v "${DIR}"/hotfixes:/usr/local/share/jasperserver-pro/customization \
-    -p 3306 "${JASPERSERVER_PRO_CMDLINE_IMAGE_NAME}" init
+# uncomment if you need to initialize the server - should only be needed when upgrading server version
+#echo "Run jasperserver pro cmdline image."
+#docker run -d --env-file="${DIR}"/.env \
+#    --name "${JASPERSERVER_PRO_CMDLINE_CONTAINER_NAME}" \
+#    -v "${DIR}"/hotfixes:/usr/local/share/jasperserver-pro/customization \
+#    -p 3306 "${JASPERSERVER_PRO_CMDLINE_IMAGE_NAME}" init
 
 echo "Run jasperserver pro image."
 docker run -d --env-file="${DIR}"/.env \
     --name="${JASPERSERVER_PRO_CONTAINER_NAME}" \
     -v "${DIR}"/hotfixes:/usr/local/share/jasperserver-pro/customization \
     -v "${DIR}"/output:/output \
+    --net=host \
     -p 3306 -p 8080:8080 "${JASPERSERVER_PRO_IMAGE_NAME}"
 
